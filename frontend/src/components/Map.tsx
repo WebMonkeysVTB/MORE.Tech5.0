66221ea45258d83d1804759a0af39d9790efe945
Deni Shaipov <deni.shaipp@gmail.com>
2023-10-04 14:58:00 +0300

    now showing departments that fit into current zoom

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -13,31 +13,41 @@ function Map({ departments }: MapProps) {
 
     const myMap = new ymaps.Map("map", {
       center: [latitude, longitude],
-      zoom: 12,
+      zoom: 15,
     });
 
     console.warn(departments)
 
-    for (let department of departments) {
-      myMap.geoObjects.add(
-        new ymaps.Placemark(
-          [department.coordinates.latitude, department.coordinates.longitude],
-          {
-            balloonContentHeader: department.shortName,
-            // Зададим содержимое основной части балуна.
-            balloonContentBody: `<div>${department.address}</div>`,
-            // Зададим содержимое нижней части балуна.
-            balloonContentFooter: `<div>Физ. лица: ${department.scheduleFl}</div><div>Юр. лица: ${department.scheduleJurL}</div>`,
-            // Зададим содержимое всплывающей подсказки.
-            hintContent: department.shortName
-          },
-          {
-            preset: "islands#icon",
-            iconColor: "#0095b6",
-          }
-        )
-      );
-    }
+    const mapObjects = ymaps.geoQuery(departments.map(department => {
+      return new ymaps.Placemark(
+        [department.coordinates.latitude, department.coordinates.longitude],
+        {
+          balloonContentHeader: department.shortName,
+          // Зададим содержимое основной части балуна.
+          balloonContentBody: `<div>${department.address}</div>`,
+          // Зададим содержимое нижней части балуна.
+          balloonContentFooter: `<div>Физ. лица: ${department.scheduleFl}</div><div>Юр. лица: ${department.scheduleJurL}</div>`,
+          // Зададим содержимое всплывающей подсказки.
+          hintContent: department.shortName
+        },
+        {
+          preset: "islands#icon",
+          iconColor: "#0095b6",
+        }
+      )
+    }));
+
+    // Найдем объекты, попадающие в видимую область карты.
+    mapObjects.searchInside(myMap)
+    // И затем добавим найденные объекты на карту.
+    .addToMap(myMap);
+
+    myMap.events.add('boundschange', function () {
+      // После каждого сдвига карты будем смотреть, какие объекты попадают в видимую область.
+      var visibleObjects = mapObjects.searchInside(myMap).addToMap(myMap);
+      // Оставшиеся объекты будем удалять с карты.
+      mapObjects.remove(visibleObjects).removeFromMap(myMap);
+  });
   }
   ymaps.ready(init);
   return (
